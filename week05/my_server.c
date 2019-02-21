9265a86704f1777d5acf6b2a660c2cfec5d8482e
Vladik <qvviko@gmail.com>
2019-02-21 21:46:39 +0300

    + threads

--- week05/my_server.c
+++ week05/my_server.c
@@ -10,50 +10,41 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+#include "pthread.h"
+#include "time.h"
 
 #define SERVER_PORT     2000 //Server will be running on this port number
 #define TRUE 1
 #define BUF_SIZE 1024
+#define THREAD_N 10
 
-int setup_connection() {
-    int server_socket;
-    char data_buf[BUF_SIZE];
-    ssize_t bytes_received, bytes_sent;
-    struct sockaddr_in server_addr, client_addr;
-    socklen_t client_addr_len = 0;
+struct handle_data {
+    int socket_id;
+    int thread_id;
+};
 
-
-    if ((server_socket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1) {
-        fprintf(stderr, "failed to create a socket errno: %d", errno);
-        return -1;
-    }
-
-    memset(&server_addr, 0, sizeof(server_addr)); //Clean up server address
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(SERVER_PORT);
-    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-
-    client_addr_len = sizeof(struct sockaddr);
-
-    if ((bind(server_socket, (struct sockaddr *) &server_addr, sizeof(struct sockaddr)) == -1)) {
-        fprintf(stderr, "failed to bind errno: %d", errno);
-        return -1;
-    }
-
-    //Ready to receive data
+void *handle_socket(void *data) {
     while (TRUE) {
-        printf("Blocked on receive call\n");
-        fflush(stdout);
+        struct handle_data *d = (struct handle_data *) data;
+        char data_buf[BUF_SIZE];
+        socklen_t client_addr_len = 0;
+        client_addr_len = sizeof(struct sockaddr);
+        ssize_t bytes_received, bytes_sent;
+        struct sockaddr_in client_addr;
 
-        if ((bytes_received = recvfrom(server_socket, data_buf, sizeof(data_buf), 0,
+
+        printf("Thread %d blocked on receive call\n", d->thread_id);
+        fflush(stdout);
+        if ((bytes_received = recvfrom(d->socket_id, data_buf, sizeof(data_buf), 0,
                                        (struct sockaddr *) &client_addr, &client_addr_len)) == -1) {
             fprintf(stderr, "failed to receive data errno: %d", errno);
-            return -1;
+            exit(EXIT_FAILURE);
         }
-        printf("Server recvd %lu bytes from client %s:%u\n", bytes_received,
-               inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
-
+        time_t clk = time(NULL);
         test_struct_t *client_data = (test_struct_t *) data_buf;
+        printf("Thread %d. Client data: Name: %s Age: %d Group_N: %d. Timestamp:%s", d->thread_id, client_data->name,
+               client_data->age,
+               client_data->group_number, ctime(&clk));
 
         char buf[20];
         result_struct_t result;
@@ -79,14 +70,48 @@ int setup_connection() {
         cur_int += strlen(buf);
         result.result[cur_int] = ')';
         result.result[cur_int + 1] = '\0';
-        printf("Beginning to sent bytes\n");
-        if ((bytes_sent = sendto(server_socket, &result, sizeof(result_struct_t), 0,
+        sleep(10);
+        clk = time(NULL);
+        printf("Thread %d, has slept. Result to send: %s. Timestamp:%s", d->thread_id, result.result, ctime(&clk));
+        if ((bytes_sent = sendto(d->socket_id, &result, sizeof(result_struct_t), 0,
                                  (struct sockaddr *) &client_addr, sizeof(struct sockaddr))) == -1) {
             fprintf(stderr, "failed to sent data errno: %d", errno);
-            return -1;
+            exit(EXIT_FAILURE);
         }
-        printf("No of bytes sent = %lu\n", bytes_sent);
+    }
+}
 
+int setup_connection() {
+    int server_socket;
+    struct sockaddr_in server_addr;
+    pthread_t threads[THREAD_N];
+
+
+    if ((server_socket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1) {
+        fprintf(stderr, "failed to create a socket errno: %d", errno);
+        return -1;
+    }
+
+    memset(&server_addr, 0, sizeof(server_addr)); //Clean up server address
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(SERVER_PORT);
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+
+    if ((bind(server_socket, (struct sockaddr *) &server_addr, sizeof(struct sockaddr)) == -1)) {
+        fprintf(stderr, "failed to bind errno: %d", errno);
+        return -1;
+    }
+
+    for (int i = 0; i < THREAD_N; i++) {
+        struct handle_data *data = (struct handle_data *) malloc(sizeof(struct handle_data));
+        data->thread_id = i + 1;
+        data->socket_id = server_socket;
+        pthread_create(&threads[i], NULL, handle_socket, (void *) data);
+    }
+
+    for (int j = 0; j < THREAD_N; j++) {
+        pthread_join(threads[j], NULL);
     }
 }
 
