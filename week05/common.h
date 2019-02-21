c1afacd34a70a12902ed277c0d41534d97928ded
Vladik <qvviko@gmail.com>
2019-02-21 16:40:16 +0300

    before changing server

---
+++ week05/common.h
@@ -0,0 +1,12 @@
+typedef struct _test_struct {
+    char name[30];
+    unsigned int age;
+    unsigned int group_number;
+
+
+} test_struct_t;
+
+
+typedef struct result_struct_ {
+    char result[100];
+} result_struct_t;
---
+++ week05/server.c
@@ -0,0 +1,133 @@
+﻿//Taken from Abhishek Sagar
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <memory.h>
+#include <errno.h>
+#include "common.h"
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+/*Server process is running on this port no. Client has to send data to this port no*/
+#define SERVER_PORT     2000
+
+test_struct_t test_struct;
+result_struct_t res_struct;
+char data_buffer[1024];
+
+void setup_tcp_server_communication() {
+
+    /*Step 1 : Initialization*/
+    /*Socket handle and other variables*/
+    /*Master socket file descriptor, used to accept new client connection only, no data exchange*/
+    int master_socket = 0;
+    ssize_t sent_recv_bytes = 0;
+    socklen_t addr_len = 0;
+
+    /*client specific communication socket file descriptor,
+     * used for only data exchange/communication between client and server*/
+    int comm_socket_fd = 0;
+    /*variables to hold server information*/
+    struct sockaddr_in server_addr, /*structure to store the server and client info*/
+            client_addr;
+
+    /*step 2: udp master socket creation*/
+    if ((master_socket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1) {
+        printf("socket creation failed\n");
+        exit(1);
+    }
+
+    /*Step 3: specify server Information*/
+    server_addr.sin_family = AF_INET;/*This socket will process only ipv4 network packets*/
+    server_addr.sin_port = SERVER_PORT;/*Server will process any data arriving on port no 2000*/
+
+    /*3232249957; //( = 192.168.56.101); Server's IP address,
+    //means, Linux will send all data whose destination address = address of any local interface
+    //of this machine, in this case it is 192.168.56.101*/
+
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+
+    addr_len = sizeof(struct sockaddr);
+
+    /* Bind the server. Binding means, we are telling kernel(OS) that any data
+     * you recieve with dest ip address = 192.168.56.101, and tcp port no = 2000, pls send that data to this process
+     * bind() is a mechnism to tell OS what kind of data server process is interested in to recieve. Remember, server machine
+     * can run multiple server processes to process different data and service different clients. Note that, bind() is
+     * used on server side, not on client side*/
+
+    if (bind(master_socket, (struct sockaddr *) &server_addr, sizeof(struct sockaddr)) == -1) {
+        printf("socket bind failed\n");
+        return;
+    }
+
+    /* Server infinite loop for servicing the client*/
+
+    while (1) {
+        memset(data_buffer, 0, sizeof(data_buffer));
+
+        sent_recv_bytes = recvfrom(comm_socket_fd, (char *) data_buffer, sizeof(data_buffer), 0,
+                                   (struct sockaddr *) &client_addr, &addr_len);
+
+        printf("Server recvd %lu bytes from client %s:%u\n", sent_recv_bytes,
+               inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+
+        test_struct_t *client_data = (test_struct_t *) data_buffer;
+
+        /* If the client sends a special msg to server, then server close the client connection
+         * for forever* */
+        if (client_data->name[0] == '\n') {
+
+            close(comm_socket_fd);
+            printf("Server closes connection with client : %s:%u\n", inet_ntoa(client_addr.sin_addr),
+                   ntohs(client_addr.sin_port));
+            /*Goto state machine State 1*/
+            break;/*Get out of inner while loop, server is done with this client, time to check for new connection request by executing selct()*/
+        }
+
+
+        char buf[20];
+        result_struct_t result;
+        int cur_int = 0;
+        result.result[cur_int] = '(';
+        cur_int++;
+        strcpy(result.result + cur_int, client_data->name);
+        cur_int += strlen(client_data->name);
+
+        result.result[cur_int] = ',';
+        result.result[cur_int + 1] = ' ';
+        cur_int += 2;
+        snprintf(buf, 10, "%d", client_data->age);
+        strcpy(result.result + cur_int, buf);
+        cur_int += strlen(buf);
+        result.result[cur_int] = ',';
+        result.result[cur_int + 1] = ' ';
+
+        cur_int += 2;
+        sprintf(buf, "%d", client_data->group_number);
+        strcpy(result.result + cur_int, buf);
+
+        cur_int += strlen(buf);
+        result.result[cur_int] = ')';
+        result.result[cur_int + 1] = '\0';
+
+
+        /* Server replying back to client now*/
+        sent_recv_bytes = sendto(comm_socket_fd, (char *) &result, sizeof(result_struct_t), 0,
+                                 (struct sockaddr *) &client_addr, sizeof(struct sockaddr));
+
+        printf("Server sent %lu bytes in reply to client\n", sent_recv_bytes);
+        /*Goto state machine State 3*/
+    }
+}
+
+int
+main(void) {
+
+    setup_tcp_server_communication();
+    return 0;
+}
